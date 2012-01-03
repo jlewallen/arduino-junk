43c01f2316cf66ac9b19decd5989eac02aba5d04
Jacob Lewallen <jlewallen@gmail.com>
2012-01-02 21:09:58 -0800

    System call to return free memory on Arduino...

---
+++ libraries/jlewallen/MemoryFree.h
@@ -0,0 +1,19 @@
+#ifndef MemoryFree_h
+#define MemoryFree_h
+
+extern unsigned int __bss_end;
+extern unsigned int __heap_start;
+extern void *__brkval;
+
+int memoryFree() {
+  int free_memory;
+
+  if((int)__brkval == 0)
+     free_memory = ((int)&free_memory) - ((int)&__bss_end);
+  else
+    free_memory = ((int)&free_memory) - ((int)__brkval);
+
+  return free_memory;
+}
+
+#endif
