80f99e289ba3edcadbbff76853d6e0881eebcfff
Jacob Lewallen <jlewallen@gmail.com>
2012-01-02 21:11:07 -0800

    Helper library for interacting with a 74HC595's.

---
+++ libraries/jlewallen/ShiftRegisters.h
@@ -0,0 +1,37 @@
+#ifndef ShiftRegisters_h
+#define ShiftRegisters_h
+
+#include <Arduino.h>
+
+class ShiftRegisters {
+  private:
+    int serPin;
+    int rclkPin;
+    int srclkPin;
+    int width;
+    
+  public:
+    ShiftRegisters(int serPin, int rclkPin, int srclkPin, int width)
+     : serPin(serPin), rclkPin(rclkPin), srclkPin(srclkPin), width(width) {
+    }
+    
+  public:
+    void begin() {
+      pinMode(serPin, OUTPUT);
+      pinMode(rclkPin, OUTPUT);
+      pinMode(srclkPin, OUTPUT);
+    }
+    
+    void write(const boolean *values) {
+      digitalWrite(rclkPin, LOW);
+      for(int i = width - 1; i >= 0; i--) {
+        digitalWrite(srclkPin, LOW);
+        digitalWrite(serPin, values[i]);
+        digitalWrite(srclkPin, HIGH);
+      }
+      digitalWrite(rclkPin, HIGH);
+    }
+  
+};
+
+#endif
