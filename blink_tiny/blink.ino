88e6d39b9afac8f31a9e6913b3ba968432ca2138
Jacob Lewallen <jlewallen@gmail.com>
2012-02-26 10:33:48 -0800

    Sample blink for getting my pin bearings on T24.

--- blink_tiny/blink.ino
+++ blink_tiny/blink.ino
@@ -1,13 +1,46 @@
+uint32_t timer0;
+uint32_t timer1;
+uint32_t timer2;
+uint32_t d0;
+
+class Blinker {
+private:
+  uint32_t previous;
+  uint32_t interval;
+  uint8_t pin;
+  uint8_t enabled;
+
+public:
+  Blinker(uint32_t interval, uint8_t pin) :
+    previous(0), interval(interval), pin(pin) {
+  }
+
+  void begin() {
+    pinMode(pin, OUTPUT);
+  }
+
+  void service() {
+    uint32_t now = millis();
+    if (now - previous > interval) {
+      enabled = !enabled;
+      digitalWrite(pin, enabled);
+      previous = now;
+    }
+  }
+};
+
+static Blinker b0( 500, 0);
+static Blinker b1( 250, 1);
+static Blinker b2(1000, 3);
+
 void setup() {
-  pinMode(0, OUTPUT);
-  pinMode(1, OUTPUT);
+  b0.begin();
+  b1.begin();
+  b2.begin();
 }
 
 void loop() {
-  digitalWrite(0, HIGH);
-  digitalWrite(1, LOW);
-  delay(500);
-  digitalWrite(0, LOW);
-  digitalWrite(1, HIGH);
-  delay(500);
+  b0.service();
+  b1.service();
+  b2.service();
 }
