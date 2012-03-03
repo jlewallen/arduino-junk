cdd76fb8d5b12923ff74a6b80101bd292daadf69
Jacob Lewallen <jlewallen@gmail.com>
2012-03-02 21:33:55 -0800

    Was overflowing in the PID code, also tweaking.

--- robear1/SpeedController.h
+++ robear1/SpeedController.h
@@ -1,22 +1,22 @@
 #ifndef SPEED_H
 #define SPEED_H
 
-#define SC_K_PRO              3         // proportional 
-#define SC_K_DRV              3         // derivative
-#define SC_MAX                100 * 256 // maximum
-#define SC_SCALE_VELOCITY(v)  (v * 20)
+#define SC_K_PRO(e)            ((e) * 4)
+#define SC_K_DRV(e)            ((e) / 2)
+#define SC_MAX                 (100 * 256)
+#define SC_SCALE_VELOCITY(v)   (v)
 
 class SpeedController : Servicable {
 private:
   encoding::Encoders *encoders;
   MotionController *motion;
   int16_t leftDesired;
-  int16_t leftAccumulator;
-  int16_t leftLast;
+  int32_t leftAccumulator;
+  int32_t leftLast;
 
   int16_t rightDesired;
-  int16_t rightAccumulator;
-  int16_t rightLast;
+  int32_t rightAccumulator;
+  int32_t rightLast;
 
 public:
   SpeedController(encoding::Encoders &encoders, MotionController &motion) : 
@@ -38,32 +38,36 @@ public:
   }
 
   void service() {
-    int16_t lf, rt;
+    int32_t lf = 0, rt = 0;
 
     if (leftDesired != 0) {
       lf = (SC_SCALE_VELOCITY(leftDesired) - SC_SCALE_VELOCITY(encoders->getLeftVelocity())) * 256; 
-      leftAccumulator += ((lf / SC_K_PRO) + ((lf - leftLast) / SC_K_DRV));
+      leftAccumulator += SC_K_PRO(lf) + SC_K_DRV(lf - leftLast);
       leftLast = lf; 
       if (leftAccumulator > SC_MAX) leftAccumulator = SC_MAX; 
       else if (leftAccumulator < -SC_MAX) leftAccumulator = -SC_MAX; 
     }
     else {
       leftAccumulator = 0;
+      leftLast = 0; 
     }
 
     if (rightDesired != 0) {
       rt = (SC_SCALE_VELOCITY(rightDesired) - SC_SCALE_VELOCITY(encoders->getRightVelocity())) * 256; 
-      rightAccumulator += ((rt / SC_K_PRO) + ((rt - rightLast) / SC_K_DRV)); 
+      rightAccumulator += SC_K_PRO(rt) + SC_K_DRV(rt - rightLast);
       rightLast = rt; 
       if (rightAccumulator > SC_MAX) rightAccumulator = SC_MAX; 
       else if (rightAccumulator < -SC_MAX) rightAccumulator = -SC_MAX; 
     }
     else {
       rightAccumulator = 0;
+      rightLast = 0; 
     }
 
-    printf("(%d %d) - (%d %d) -> %d %d\n\r",
+    printf("d(%d %d) (%lu %lu) last(%lu %lu) vel(%lu %lu) -> pwm(%lu %lu)\n\r",
            leftDesired, rightDesired,
+           lf, rt,
+           leftLast, rightLast,
            encoders->getLeftVelocity(), encoders->getRightVelocity(),
            leftAccumulator / 256, rightAccumulator / 256
            );
